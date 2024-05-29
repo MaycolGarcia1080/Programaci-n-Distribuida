51bbded93f3510f99b29424414ac51612e692a05
Maycol Garcia <maicolgarcia1083@gmail.com>
2024-05-29 15:51:08 -0500

    first commit

---
+++ IHUPC/src/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    database: {
+        connectionLimit: 10,
+        host: 'baapgktbpco4d3wg4v83-mysql.services.clever-cloud.com',
+        user: 'uglsuyrae50gnic6',
+        password: '4eWtHwDFMDNhCw36WEQx',
+        database: 'baapgktbpco4d3wg4v83'
+    }
+};
